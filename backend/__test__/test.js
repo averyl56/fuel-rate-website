1cac087b4ec6d9d2200a7dd2795a2168d41675a4
Rodney Hong <hongrodney0@gmail.com>
2024-03-21 15:02:30 -0500

    commit

--- backend/__test__/test.js
+++ backend/__test__/test.js
@@ -1,5 +1,24 @@
 // test.js (your test file)
 
+/* 
+ I think we'll need to add a test file for front end as well
+ To install jest: npm install --save-dev jest 
+ Also install supertest: npm install supertest
+ To test: npm test -- --coverage
+
+ describe('Login Route', () => {                        This describes what you're testing, it can be called anything
+  it('should log in successfully', async () => {        This describes what it should do
+    const response = await request(app)     
+      .post('/login')                                   Add in the route you're using
+      .send({                                           Hardcode the vals
+        username: 'johnny',
+        password: '12345'
+      });
+
+    expect(response.status).toBe(200);                  Use 200 when it should be successful submission, 404 when it should fail
+  });
+*/
+
 const request = require('supertest');
 const express = require('express');
 const router = require('../routers/quotehistory.js'); // Adjust the path to your actual quotehistory.js file
@@ -52,5 +71,297 @@ describe('Login Route', () => {
   });
 
 
+});
+
+describe('Profile Route', () => {
+  it('should submit the profile form successfully', async () => {
+    const response = await request(app)
+      .post('/profile')
+      .send({
+        // maybe need userId?
+        name: 'Archibald Humphrey',
+        address1: '4300 Martin Luther King Blvd',
+        address2: '', // address 2 is optional, dont know if we should test it both ways (blank and not blank)
+        city: 'Houston',
+        state: 'TX',
+        zipcode: '77204'
+      });
+
+      expect(response.status).toBe(200);
+  });
+
+  it('should return an error for a blank full name', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: '',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: 'Houston',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+  it('should return an error for an invalid full name', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: 'Houston',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for a blank address1', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '',
+      address2: '', 
+      city: 'Houston',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+  it('should return an error for an invalid address1', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      address2: '', 
+      city: 'Houston',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });    
+
+  it('should return an error for an invalid address2', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '4300 Martin Luther King Blvd',
+      address2: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 
+      city: 'Houston',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for a blank city', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: '',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+  it('should return an error for an invalid city', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      state: 'TX',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for a blank state', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: 'Houston',
+      state: '',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+  it('should return an error for an invalid state', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: 'Houston',
+      state: 'AU',
+      zipcode: '77204'
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for an invalid zipcode', async () => {
+    const response = await request(app)
+    .post('/profile')
+    .send({
+      name: 'Archibald Humphrey',
+      address1: '4300 Martin Luther King Blvd',
+      address2: '', 
+      city: 'Houston',
+      state: 'TX',
+      zipcode: '7720'
+    });
+
+    expect(response.status).toBe(404);
+  });
 
 });
+
+describe('Fuel Rate Route', () => {
+  it('should return a successful form', async () => {
+    const response = await request(app)
+    .post('./fuelrate')
+    .send({
+      gallonsRequested: 5000,
+      deliveryAddress: '4300 Martin Luther King Blvd', // comes from client profile, dont know how we would implement this w/ city, state, zip
+      deliveryDate: '2024-02-16',
+      suggestedPrice: 10000,
+      totalAmount: 50000000
+    });
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should return an error for an invalid gallons req', async () => {
+    const response = await request(app)
+    .post('/fuelrate')
+    .send({
+      gallonsRequested: 'a',
+      deliveryAddress: '4300 Martin Luther King Blvd', 
+      deliveryDate: '2024-02-16',
+      suggestedPrice: 10000,
+      totalAmount: 15000
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for an invalid delivery date', async () => {
+    const response = await request(app)
+    .post('/fuelrate')
+    .send({
+      gallonsRequested: 5000,
+      deliveryAddress: '4300 Martin Luther King Blvd', 
+      deliveryDate: '1776-02-16',
+      suggestedPrice: 10000,
+      totalAmount: 15000
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for when a delivery address isnt retrieved', async () => {
+    const response = await request(app)
+    .post('/fuelrate')
+    .send({
+      gallonsRequested: 5000,
+      deliveryAddress: '', 
+      deliveryDate: '1776-02-16',
+      suggestedPrice: 10000,
+      totalAmount: 15000
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for when the suggested price isnt retrieved', async () => {
+    const response = await request(app)
+    .post('/fuelrate')
+    .send({
+      gallonsRequested: 5000,
+      deliveryAddress: '4300 Martin Luther King Blvd', 
+      deliveryDate: '1776-02-16',
+      suggestedPrice: 0,
+      totalAmount: 1
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for if totalAmount isnt equal to gallons*price', async () => {
+    const response = await request(app)
+    .post('/fuelrate')
+    .send({
+      gallonsRequested: 10,
+      deliveryAddress: '4300 Martin Luther King Blvd', 
+      deliveryDate: '1776-02-16',
+      suggestedPrice: 100,
+      totalAmount: 15
+    });
+
+    expect(response.totalAmount).toBe(response.gallonsRequested*response.suggestedPrice);
+    expect(response.status).toBe(404);
+  });
+}); 
+
+describe('Signup Route', () => {
+  it('should return a successful sign up creation', async () => {
+    const response = await request(app)
+    .post('/signup')
+    .send({
+      username: 'fueltracker',
+      password: 'lepookie123'
+    });
+    expect(response.status).toBe(200);
+  });
+
+  it('should return an error for invalid username', async () => {
+    const response = await request(app)
+    .post('/signup')
+    .send({
+      username: 'fuel tracker',
+      password: 'lepookie123'
+    });
+    expect(response.status).toBe(404);
+  });
+  it('should return an error for long username', async () => {
+    const response = await request(app)
+    .post('/signup')
+    .send({
+      username: 'fueltrackerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',
+      password: 'lepookie123'
+    });
+    expect(response.status).toBe(404);
+  });
+
+  it('should return an error for an invalid password', async () => {
+    const response = await request(app)
+    .post('/signup')
+    .send({
+      username: 'fueltracker',
+      password: 'lepookie123111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
+    });
+    expect(response.status).toBe(404);
+  });
+}) 
